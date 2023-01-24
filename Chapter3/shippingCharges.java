44225b932152ef0cae9bf1eac9ad21c8422b0b59
Mistaflowas <willtf2000@gmail.com>
2023-01-24 12:29:19 -0600

    Finished question number 9 for Programming Challenges in Chapter 3

--- Chapter3/shippingCharges.java
+++ Chapter3/shippingCharges.java
@@ -8,6 +8,7 @@ The shipping charges per 500 miles are not prorated. For example, if a 2-pound p
 Write a program that asks the user to enter the weight of a package and then displays the shipping charges. */
 
 //Import classes
+import javax.lang.model.util.ElementScanner6;
 import javax.swing.JOptionPane;
 
 public class shippingCharges 
@@ -15,5 +16,46 @@ public class shippingCharges
     public static void main(String[] args)
     {
         //Declare constants and variables
-    }    
+        final int CHARGELIMIT = 500;
+
+        String input;
+        double rate;
+        double cost;
+        int distance;
+        int rateCharge;
+        int weight;
+
+        //Prompt user
+        input = JOptionPane.showInputDialog(null, "Enter the Weight, in Pounds");
+        weight = Integer.parseInt(input);
+
+        input = JOptionPane.showInputDialog(null, "Enter distance Shipped, in Miles.");
+        distance = Integer.parseInt(input);
+
+        //Calculate
+        //How many times rate is charged
+        rateCharge = distance / CHARGELIMIT;
+            //Make sure rateCharge is at least 1
+            if(rateCharge < 1)
+                rateCharge = 1;
+
+        //Rate by Weight
+        if(weight >= 10)
+            rate = 3.8;
+        else if(weight >= 6)
+            rate = 3.7;
+        else if(weight >= 2)
+            rate = 2.2;
+        else
+            rate = 1.1;
+
+        //Cost
+        cost = rateCharge * rate;
+
+        //Display
+        JOptionPane.showMessageDialog(null,String.format("Shipping Charges are $%,.2f", cost));
+
+        //Close the program
+        System.exit(0);
+    }
 }
